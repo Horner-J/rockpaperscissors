2bc62c7ecc58aa6c02fc79820fb3bdcd5f0893a3
Horner-J <jasonandrewhorner@gmail.com>
2022-05-17 12:37:21 -0600

    Adding function to evaluate win conditions

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,14 +1,30 @@
-const choices = ["rock", "paper", "scissors"];
+
+const choices = ["rock", "paper", "scissors"];//Creates array of possible choices
 
 function computerPlay() {
-    return choices[Math.floor(Math.random()*choices.length)];
+    return choices[Math.floor(Math.random()*choices.length)];//Randomizes computer choice
 }
 
+const computerSelection = computerPlay();
+console.log(computerSelection)
+
 function playerChoice() {
-    let input = prompt("Type Rock, Paper or Scissors");
+    let input = prompt("Type Rock, Paper or Scissors");//Prompts user for input
     while(input == null){
-        input = prompt("Type Rock, Paper or Scissors");
+        input = prompt("Type Rock, Paper or Scissors");//Keeps prompt box persistent until choice is made 
     }
-    input = input.toLowerCase();
+    input = input.toLowerCase(); //Makes player choice case-insensitive
     console.log(input);
 }
+
+const playerSelection = playerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+   if (playerSelection === computerSelection) {
+       return "tie!";
+   } 
+}    
+console.log(playRound()); 
+
+  
